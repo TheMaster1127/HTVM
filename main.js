c8764cc0b8b6549edf70118b5c5383524f0e3787
TheMaster1127 <Dzemilefere@gmail.com>
2024-09-22 14:05:12 +0300

    fixed some bugs

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function MsgBox(value) {
 
 
 
+var qWASED;
 let arr123 = [];
 arr123.push(6);
 arr123.push(5);
@@ -18,7 +19,16 @@ for (let A_Index1 = 1; A_Index1 <= arr123.length + 1 - 1; A_Index1++) {
 for (let A_Index2 = 1; A_Index2 <= 3 + 1 - 1; A_Index2++) {
     MsgBox(A_Index2);
 }
+while (AWSEDS && WER) {
+    aQwsde
+}
+let result = (age >= 18) ? "Adult" : "Minor";
 // You can use indentations if you want to. It's all optional.
 if (arr123[1] == 5) {
     MsgBox("yes");
+}
+else if (asd) {
+    AwSZDF
+} else {
+    aszd
 }
