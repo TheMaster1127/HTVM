95384041a6942e2eaf8ee8100adbcd33112d9e62
TheMaster1127 <Dzemilefere@gmail.com>
2025-02-24 17:51:57 +0200

    added HTVM to the online-ide.html

---
+++ src-website/htvm-mode.js
@@ -0,0 +1,49 @@
+// Define the HTVM mode for Ace Editor
+ace.define("ace/mode/htvm", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(require, exports, module) {
+    var oop = require("ace/lib/oop");
+    var TextMode = require("ace/mode/text").Mode;
+    var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+
+    var HTVMHighlightRules = function() {
+        var htvmKeywords = "if|else if|else|def obj|prop|crew|alliance|method|while|for|Loop|Loop,|Loop, Parse,|continue|break|func|await|switch|case|throw|try|catch|finally|var|let|const|return|end";
+        var builtInCommands = "StringTrimLeft|StringTrimRight|Random|Sleep|FileRead|FileAppend|FileDelete|Sort|MsgBox";
+        var builtInFunctions = "HTVM_func1|LoopParseFunc|print|STR|INT|FLOAT|InStr|Random|Sleep|input|FileRead|FileAppend|FileDelete|StrLen|Asc|Abs|ACos|ASin|ATan|Ceil|Cos|Exp|Ln|Log|Round|Sin|Sqrt|Tan|SubStr|Trim|StrReplace|StringTrimLeft|StringTrimRight|StrLower|StrSplit|Chr|Mod|Floor|Sort|getDataFromAPI|FileCreateDir|FileRemoveDir|FileExist|isWindows|GetParams|BuildInVars|RegExReplace|RunCMD|RegExMatch|ExitApp|Gui_Show|Gui_Update|Gui_Button|Gui_Edit|Gui_Text|Gui_Size|Gui_Font|GuiControl|MakeHotKey|sortArr|countChars|StrTitleCase|createBackendInit|addEndpoint|startServer|getDataFromEndpoint|HTVM_getLang_HTVM|ErrorMsg";
+        var staticTypes = "int|str|void|bool|float|int8|int16|int32|int64|arr|arr int|arr str|arr float|arr bool";
+        var builtInVars = "A_Index|A_LoopField|this";
+        var arrayMethods = "add|pop|size|insert|rm|indexOf";
+
+        this.$rules = {
+            start: [
+                { token: "comment", regex: ";.*$" },
+                { token: "comment.block", regex: /'''1/, next: "comment_block" },
+                { token: "keyword", regex: "\\b(?:" + htvmKeywords + ")\\b" },
+                { token: "command", regex: "\\b(?:" + builtInCommands + ")(?=\\,)" },
+                { token: "functions", regex: "\\b(?:" + builtInFunctions + ")(?=\\()" },
+                { token: "BuildInFunc", regex: "\\b(?:" + builtInVars + ")\\b" },
+                { token: "arrayMethods", regex: "\\.(?:" + arrayMethods + ")\\b" },
+                { token: "static_types", regex: "\\b(?:" + staticTypes + ")\\b" },
+                { token: "operators", regex: "&&|\\|\\||and|or" },
+                { token: "trueANDfalse", regex: "\\b(true|false|global)\\b" },
+                { token: "variables", regex: "\\b[a-zA-Z_][a-zA-Z0-9_]*\\b" },
+                { token: "constant.numeric", regex: "\\b[0-9]+\\b" },
+                { token: "braces_Open", regex: "\\{" },
+                { token: "braces_Close", regex: "\\}" },
+                { token: "string", regex: '".*?"' }
+            ],
+            comment_block: [
+                { token: "comment.block", regex: /.*?'''2/, next: "start" },
+                { token: "comment.block", regex: /.*/ }
+            ]
+        };
+    };
+
+    oop.inherits(HTVMHighlightRules, TextHighlightRules);
+
+    var Mode = function() {
+        this.HighlightRules = HTVMHighlightRules;
+    };
+    oop.inherits(Mode, TextMode);
+
+    exports.Mode = Mode;
+});
+
---
+++ src-website/htvm_completions.js
@@ -0,0 +1,38 @@
+
+
+let HTVM_Syntax_AutoComplete = [{ name: "if" }, { name: "else if" }, { name: "else" }, { name: "def obj" }, { name: "prop" }, { name: "crew" }, { name: "alliance" }, { name: "method" }, { name: "while" }, { name: "for" }, { name: "Loop" }, { name: "Loop," }, { name: "Loop, Parse," }, { name: "continue" }, { name: "break" }, { name: "func" }, { name: "await" }, { name: "switch" }, { name: "case" }, { name: "throw" }, { name: "try" }, { name: "catch" }, { name: "finally" }, { name: "var" }, { name: "let" }, { name: "const" }, { name: "return" }, { name: "end" }, { name: "StringTrimLeft" }, { name: "StringTrimRight" }, { name: "Random" }, { name: "Sleep" }, { name: "FileRead" }, { name: "FileAppend" }, { name: "FileDelete" }, { name: "Sort" }, { name: "MsgBox" }, { name: "HTVM_func1" }, { name: "HTVM_func1" }, { name: "HTVM_func1" }, { name: "LoopParseFunc" }, { name: "LoopParseFunc" }, { name: "LoopParseFunc" }, { name: "print" }, { name: "print" }, { name: "print" }, { name: "STR" }, { name: "INT" }, { name: "FLOAT" }, { name: "STR" }, { name: "INT" }, { name: "FLOAT" }, { name: "INT" }, { name: "STR" }, { name: "FLOAT" }, { name: "InStr" }, { name: "Random" }, { name: "Sleep" }, { name: "input" }, { name: "FileRead" }, { name: "FileAppend" }, { name: "FileDelete" }, { name: "StrLen" }, { name: "Asc" }, { name: "Abs" }, { name: "ACos" }, { name: "ASin" }, { name: "ATan" }, { name: "Ceil" }, { name: "Cos" }, { name: "Exp" }, { name: "Ln" }, { name: "Log" }, { name: "Round" }, { name: "Sin" }, { name: "Sqrt" }, { name: "Tan" }, { name: "SubStr" }, { name: "Trim" }, { name: "StrReplace" }, { name: "StringTrimLeft" }, { name: "StringTrimRight" }, { name: "StrLower" }, { name: "StrSplit" }, { name: "Chr" }, { name: "Mod" }, { name: "Floor" }, { name: "Sort" }, { name: "getDataFromAPI" }, { name: "FileCreateDir" }, { name: "FileRemoveDir" }, { name: "FileExist" }, { name: "isWindows" }, { name: "GetParams" }, { name: "BuildInVars" }, { name: "RegExReplace" }, { name: "RunCMD" }, { name: "RegExMatch" }, { name: "ExitApp" }, { name: "InStr" }, { name: "InStr" }, { name: "Random" }, { name: "Random" }, { name: "Sleep" }, { name: "Sleep" }, { name: "input" }, { name: "input" }, { name: "FileAppendJs" }, { name: "FileDelete" }, { name: "FileAppend" }, { name: "FileRead" }, { name: "StrLen" }, { name: "StrLen" }, { name: "Chr" }, { name: "Chr" }, { name: "Asc" }, { name: "Abs" }, { name: "ACos" }, { name: "ASin" }, { name: "ATan" }, { name: "Ceil" }, { name: "Cos" }, { name: "Exp" }, { name: "Ln" }, { name: "Log" }, { name: "Round" }, { name: "Sin" }, { name: "Sqrt" }, { name: "Tan" }, { name: "Asc" }, { name: "Abs" }, { name: "ACos" }, { name: "ASin" }, { name: "ATan" }, { name: "Ceil" }, { name: "Cos" }, { name: "Exp" }, { name: "Ln" }, { name: "Log" }, { name: "Round" }, { name: "Sin" }, { name: "Sqrt" }, { name: "Tan" }, { name: "SubStr" }, { name: "SubStr" }, { name: "Trim" }, { name: "StrReplace" }, { name: "StringTrimLeft" }, { name: "StringTrimRight" }, { name: "StrLower" }, { name: "StrSplit" }, { name: "Mod" }, { name: "Floor" }, { name: "Trim" }, { name: "StrReplace" }, { name: "StringTrimLeft" }, { name: "StringTrimRight" }, { name: "StrLower" }, { name: "StrSplit" }, { name: "Mod" }, { name: "Floor" }, { name: "Sort" }, { name: "getDataFromAPI" }, { name: "FileCreateDir" }, { name: "FileRemoveDir" }, { name: "FileExist" }, { name: "isWindows" }, { name: "GetParams" }, { name: "BuildInVars" }, { name: "RegExReplace" }, { name: "RunCMD" }, { name: "RegExMatch" }, { name: "ExitApp" }, { name: "Sort" }, { name: "getDataFromAPI" }, { name: "getUrlParams" }, { name: "BuildInVars" }, { name: "RegExReplace" }, { name: "RegExMatch" }, { name: "MsgBox" }, { name: "Gui_Show" }, { name: "Gui_Update" }, { name: "Gui_Button" }, { name: "Gui_Edit" }, { name: "Gui_Text" }, { name: "Gui_Size" }, { name: "Gui_Font" }, { name: "Gui_Font_Id" }, { name: "Gui_Size_Id" }, { name: "GuiControl" }, { name: "Gui_DropDownList" }, { name: "Gui_Hide" }, { name: "Gui_Move" }, { name: "Gui_Color" }, { name: "Gui_Image" }, { name: "Gui_Toggle" }, { name: "Gui_CheckBox" }, { name: "MakeHotKey" }, { name: "sortArr" }, { name: "sortArr" }, { name: "sortArr" }, { name: "countChars" }, { name: "countChars" }, { name: "countChars" }, { name: "StrTitleCase" }, { name: "StrTitleCase" }, { name: "StrTitleCase" }, { name: "createBackendInit" }, { name: "addEndpoint" }, { name: "startServer" }, { name: "getDataFromEndpoint" }, { name: "HTVM_getLang_HTVM" }, { name: "HTVM_getLang_HTVM" }, { name: "HTVM_getLang_HTVM" }, { name: "ErrorMsg" }, { name: "A_LoopField" }, { name: "A_Index" }, { name: "add" }, { name: "pop" }, { name: "size" }, { name: "insert" }, { name: "rm" }, { name: "indexOf" }, { name: "int" }, { name: "str" }, { name: "void" }, { name: "bool" }, { name: "float" }, { name: "int8" }, { name: "int16" }, { name: "int32" }, { name: "int64" }, { name: "arr" }, { name: "arr int" }, { name: "arr str" }, { name: "arr float" }, { name: "arr bool" }, { name: "global" }, { name: "this" }]
+
+
+let Completer = {
+    getCompletions: function (editor, session, pos, prefix, callback) {
+        // If the prefix starts with "p", we allow autocompletion.
+        if (prefix.startsWith("p")) {
+            // Proceed with filtering completions
+        } else {
+            // If the prefix length is not more than 1, we return early
+            if (prefix.length <= 1) {
+                callback(null, []); // Empty array means no suggestions
+                return;
+            }
+        }
+
+        let prefixLower = prefix.toLowerCase();
+        let filteredTables = HTVM_Syntax_AutoComplete.filter(function (table) {
+            return table.name.toLowerCase().startsWith(prefixLower);
+        });
+
+        let limitedTables = filteredTables;
+
+        // Return the filtered and mapped completions
+        callback(
+            null,
+            limitedTables.map(function (table) {
+                return {
+                    caption: table.name,
+                    value: table.name,
+                };
+            })
+        );
+    }
+};
+
