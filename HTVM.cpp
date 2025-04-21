ec1bc2636c05ce535e94217429b1941cad106cd6
TheMaster1127 <Dzemilefere@gmail.com>
2025-04-22 00:44:28 +0300

    fixed a bug

--- HTVM.cpp
+++ HTVM.cpp
@@ -9369,11 +9369,15 @@ std::string compiler(std::string htCode, std::string allInstructionFile, std::st
                     if (SubStr(Trim(A_LoopField129), 1, StrLen(Trim(keyWordComment))) == Trim(keyWordComment) && isLineAconstruct(Trim(A_LoopField129)) == false) {
                         fixSomeCurlyBraceForExratFlexability += A_LoopField129 + Chr(10);
                     } else {
-                        str1 = StrReplace(A_LoopField129, keyWordCurlyBraceOpen, "{");
-                        str1 = StrReplace(str1, keyWordCurlyBraceClose, "}");
-                        str1 = StrReplace(str1, "{", Chr(10) + "{" + Chr(10));
-                        str1 = StrReplace(str1, "}", Chr(10) + "}" + Chr(10));
-                        fixSomeCurlyBraceForExratFlexability += str1 + Chr(10);
+                        if (InStr(A_LoopField129, " " + keyWordAssign + " ") || InStr(A_LoopField129, " " + keyWordAssignAdd + " ") || InStr(A_LoopField129, " " + keyWordAssignConcat + " ") || InStr(A_LoopField129, " " + keyWordAssignSub + " ") || InStr(A_LoopField129, " " + keyWordAssignMul + " ") || InStr(A_LoopField129, " " + keyWordAssignDiv + " ") || InStr(A_LoopField129, " " + keyWordAssignMod + " ") || InStr(A_LoopField129, " " + keyWordAssignShiftLeft + " ") || InStr(A_LoopField129, " " + keyWordAssignShiftRight + " ") || InStr(A_LoopField129, " " + keyWordLogicalAssignShiftRight + " ") || InStr(A_LoopField129, " " + keyWordAssignBitAnd + " ") || InStr(A_LoopField129, " " + keyWordAssignBitOr + " ") || InStr(A_LoopField129, " " + keyWordAssignBitXor + " ") && useCurlyBracesSyntaxForArrayDef == "on") {
+                            fixSomeCurlyBraceForExratFlexability += A_LoopField129 + Chr(10);
+                        } else {
+                            str1 = StrReplace(A_LoopField129, keyWordCurlyBraceOpen, "{");
+                            str1 = StrReplace(str1, keyWordCurlyBraceClose, "}");
+                            str1 = StrReplace(str1, "{", Chr(10) + "{" + Chr(10));
+                            str1 = StrReplace(str1, "}", Chr(10) + "}" + Chr(10));
+                            fixSomeCurlyBraceForExratFlexability += str1 + Chr(10);
+                        }
                     }
                 } else {
                     fixSomeCurlyBraceForExratFlexability += A_LoopField129 + Chr(10);
@@ -9566,10 +9570,10 @@ std::string compiler(std::string htCode, std::string allInstructionFile, std::st
                 std::string A_LoopField143 = items143[A_Index143 - 0];
                 if (InStr(A_LoopField143, " " + keyWordAssign + " ") || InStr(A_LoopField143, " " + keyWordAssignAdd + " ") || InStr(A_LoopField143, " " + keyWordAssignConcat + " ") || InStr(A_LoopField143, " " + keyWordAssignSub + " ") || InStr(A_LoopField143, " " + keyWordAssignMul + " ") || InStr(A_LoopField143, " " + keyWordAssignDiv + " ") || InStr(A_LoopField143, " " + keyWordAssignMod + " ") || InStr(A_LoopField143, " " + keyWordAssignShiftLeft + " ") || InStr(A_LoopField143, " " + keyWordAssignShiftRight + " ") || InStr(A_LoopField143, " " + keyWordLogicalAssignShiftRight + " ") || InStr(A_LoopField143, " " + keyWordAssignBitAnd + " ") || InStr(A_LoopField143, " " + keyWordAssignBitOr + " ") || InStr(A_LoopField143, " " + keyWordAssignBitXor + " ") && useCurlyBracesSyntaxForArrayDef == "on") {
                     if (SubStr(StrLower(A_LoopField143), 1, StrLen(StrLower(keyWordFunc))) == StrLower(keyWordFunc) || SubStr(StrLower(A_LoopField143), 1, StrLen(StrLower(keyWordAsync) + StrLower(keyWordFunc))) == StrLower(keyWordAsync) + StrLower(keyWordFunc)) {
-                        if (InStr(Trim(A_LoopField143), "{") && Trim(A_LoopField143) != "{" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment) && isLineAconstruct(Trim(A_LoopField143)) == true) {
+                        if (InStr(Trim(A_LoopField143), "{") && Trim(A_LoopField143) != "{" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment)) {
                             outCodeFixBraces += Trim(StrReplace(Trim(A_LoopField143), "{", "")) + "\n{\n";
                         }
-                        else if (InStr(Trim(A_LoopField143), "}") && Trim(A_LoopField143) != "}" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment) && isLineAconstruct(Trim(A_LoopField143)) == true) {
+                        else if (InStr(Trim(A_LoopField143), "}") && Trim(A_LoopField143) != "}" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment)) {
                             outCodeFixBraces += "}\n" + Trim(StrReplace(Trim(A_LoopField143), "}", "")) + Chr(10);
                         } else {
                             outCodeFixBraces += Trim(A_LoopField143) + Chr(10);
@@ -9578,10 +9582,10 @@ std::string compiler(std::string htCode, std::string allInstructionFile, std::st
                         outCodeFixBraces += Trim(A_LoopField143) + Chr(10);
                     }
                 } else {
-                    if (InStr(Trim(A_LoopField143), "{") && Trim(A_LoopField143) != "{" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment) && isLineAconstruct(Trim(A_LoopField143)) == true) {
+                    if (InStr(Trim(A_LoopField143), "{") && Trim(A_LoopField143) != "{" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment)) {
                         outCodeFixBraces += Trim(StrReplace(Trim(A_LoopField143), "{", "")) + "\n{\n";
                     }
-                    else if (InStr(Trim(A_LoopField143), "}") && Trim(A_LoopField143) != "}" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment) && isLineAconstruct(Trim(A_LoopField143)) == true) {
+                    else if (InStr(Trim(A_LoopField143), "}") && Trim(A_LoopField143) != "}" && SubStr(Trim(A_LoopField143), 1, StrLen(Trim(keyWordComment))) != Trim(keyWordComment)) {
                         outCodeFixBraces += "}\n" + Trim(StrReplace(Trim(A_LoopField143), "}", "")) + Chr(10);
                     } else {
                         outCodeFixBraces += Trim(A_LoopField143) + Chr(10);
